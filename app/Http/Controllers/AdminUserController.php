f5d9ddc3b443f4cfec27bf61c00ce1d619bd011d
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2018-06-26 11:49:09 +0200

    users with photos working

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\UsersRequest;
 use Illuminate\Http\Request;
 use App\User;
 use App\Role;
+use App\Photo;
 
 use App\Http\Requests;
 
@@ -44,7 +45,24 @@ class AdminUserController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        User::create($request->all());
+
+        $input = $request->all();
+        
+        if($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+            
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
         return redirect('/admin/users');
 
         // return $request->all();
